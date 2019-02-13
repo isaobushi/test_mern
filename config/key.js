1aa0abccb069db61eee96b6f07d84b47265ff74b
Andrea Mele <andrea.mele.au@gmail.com>
2019-02-13 16:29:06 +1100

    user auth

--- config/key.js
+++ config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
 	mongoURI: 'mongodb://admin:admin84@ds133875.mlab.com:33875/test_mern',
+	secretOrKey: 'secret',
 };
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const users = require('./api/users');
-const profile = require('./api/profile');
-const posts = require('./api/posts');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
+
+// body-parser middleware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 
@@ -14,17 +20,22 @@ const db = require('./config/key').mongoURI;
 //connect to mongoDB
 
 mongoose
-	.connect(db)
+	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log('MongoDB connected'))
 	.catch(err => console.log(err));
 
+// Passport middleeare
+app.use(passport.initialize());
+
+// Passport Config
+
+require('./config/passport')(passport);
 // use routes
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-app.get('/', (req, res) => res.send('Hello'));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
