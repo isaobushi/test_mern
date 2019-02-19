8fef87370445fc2fac66eba8db7ec849f9723c00
Andrea Mele <andrea.mele.au@gmail.com>
2019-02-19 23:45:08 +1100

    redux login

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import './App.css';
+
+//check for token
+
+if (localStorage.jwtToken) {
+	//set the authToken header off
+	setAuthToken(localStorage.jwtToken);
+	// decode token and get user info and exp
+	const decoded = jwt_decode(localStorage.jwtToken);
+	// set user and isAuthenticated
+	store.dispatch(setCurrentUser(decoded));
+	//check for expired token
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		//logout the user
+		store.dispatch(logoutUser());
+		//TODO! :: clear current profile
+		// redirect to login
+		window.location.href = '/login';
+	}
+}
+
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div className="App">
-					<Navbar />
-					<Route exact path="/" component={Landing} />
-					<div className="container">
-						<Route exact path="/register" component={Register} />
-						<Route exact path="/login" component={Login} />
+			<Provider store={store}>
+				<Router>
+					<div className="App">
+						<Navbar />
+						<Route exact path="/" component={Landing} />
+						<div className="container">
+							<Route exact path="/register" component={Register} />
+							<Route exact path="/login" component={Login} />
+						</div>
+						<Footer />
 					</div>
-					<Footer />
-				</div>
-			</Router>
+				</Router>
+			</Provider>
 		);
 	}
 }
